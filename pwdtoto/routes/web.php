b33b6dd7673ac75960c870d7167ba4b297d7b9b5
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-06-09 11:59:16 -0500

    Limpieza

--- pwdtoto/routes/web.php
+++ pwdtoto/routes/web.php
@@ -4,32 +4,17 @@ use App\Http\Controllers\AcercaDeController;
 use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\ServiciosController;
 use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::get('/', [HomeController::class, 'home'])->name('home');
-
 // Route:: controller(ServiciosController::class)->group(function(){
-
 //     Route::get('/servicios','index');
 //     Route::get('/servicios/create','create');
 //     Route::get('/servicios/{servicios}','show');
 //     Route::get('/servicios/p','p');
 // }); 
-
 Route::get('/servicios', [ServiciosController::class, 'index'])->name('index');
 Route::get('/servicios/create', [ServiciosController::class, 'create'])->name('create');
 Route::get('/servicios/show', [ServiciosController::class, 'show'])->name('show');
 Route::get('/servicios/p', [ServiciosController::class, 'p'])->name('p');
-
-
 
 Route::get('/contacto', [ContactoController::class, 'contacto'])->name('contacto');
 Route::get('/acercade', [AcercaDeController::class, 'acercade'])->name('acercade');
