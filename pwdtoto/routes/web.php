01f6638c50041c3492762f5c3711879171be8379
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-06-18 18:32:28 -0500

    Login

--- pwdtoto/routes/web.php
+++ pwdtoto/routes/web.php
@@ -1,16 +1,14 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AcercaDeController;
 use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\ServiciosController;
 use App\Http\Controllers\HomeController;
+
 Route::get('/', [HomeController::class, 'home'])->name('home');
-// Route:: controller(ServiciosController::class)->group(function(){
-//     Route::get('/servicios','index');
-//     Route::get('/servicios/create','create');
-//     Route::get('/servicios/{servicios}','show');
-//     Route::get('/servicios/p','p');
-// }); 
+
 Route::get('/servicios', [ServiciosController::class, 'index'])->name('index');
 Route::get('/servicios/create', [ServiciosController::class, 'create'])->name('create');
 Route::get('/servicios/show', [ServiciosController::class, 'show'])->name('show');
@@ -18,3 +16,9 @@ Route::get('/servicios/p', [ServiciosController::class, 'p'])->name('p');
 
 Route::get('/contacto', [ContactoController::class, 'contacto'])->name('contacto');
 Route::get('/acercade', [AcercaDeController::class, 'acercade'])->name('acercade');
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
+
+Route::post('/login', [UserController::class, 'login'])->name('login');
